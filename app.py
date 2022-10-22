6c299a3b287a0a35063a80c0191850e5c253790e
Dylan Chou <dylanchou@bosm-zzhou.mynetworksettings.com>
2022-10-22 01:09:35 -0400

    Modified app.py
    Adding back landing page

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
-#import streamlit as st
-import os
-#import api
+import streamlit as st
+import api
 
-#def landing_page():
-#    st.write("# Welcome to the ML model preparation tool!")
-#    st.markdown(
-#        """
-#        Data Labs' NLP toolkit has two features: embedding evaluation 
-#and load balancing on optimally storing large data for ML models.
-#        """
-#    )
+def landing_page():
+    st.write("# Welcome to the ML model preparation tool!")
+    st.markdown(
+        """
+        Data Labs' NLP toolkit has two features: embedding evaluation 
+        and load balancing on optimally storing large data for ML models.
+        """
+    )
 
 if __name__ == "__main__":
-    #landing_page()
-    pass
+    landing_page()
