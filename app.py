ee23e1167c258c0feefe1ac6d7ed6bbbe08c10e2
Dylan Chou <dylanchou@bosm-zzhou.mynetworksettings.com>
2022-10-21 23:24:42 -0400

    Adding app file and requirements
    First commit - app.py shows one page (landing)
    and requirements should contain most of the necessary
    packages for the webapp (obviously streamlit)

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+def landing_page():
+    st.write("# Welcome to the ML model preparation tool!")
+    st.markdown(
+        """
+        Data Labs' NLP toolkit has two features: embedding evaluation 
+and load balancing on optimally storing large data for ML models.
+        """
+    )
+
+if __name__ == "__main__":
+    landing_page()
